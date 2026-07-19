b60be5d5c05cf759a560136bf8c641bea0b76743
Your GitHub Username <fadel_h669@hotmail.com>
2026-07-19 15:55:16 +0300

    fix folder storage destination name

--- backend/src/applications/applications.controller.ts
+++ backend/src/applications/applications.controller.ts
@@ -11,9 +11,9 @@ import { multerConfig } from 'common/multer.config.js';
 @Controller('applications')
 export class ApplicationsController {
   constructor(private applicationService: ApplicationsService) {}
-  
+
   @Post('upload')
-  @UseInterceptors(FileInterceptor('cv', multerConfig))
+  @UseInterceptors(FileInterceptor('cvs', multerConfig))
   uploadCv(@UploadedFile() file: Express.Multer.File) {
     return {
       url: `/uploads/cvs/${file.filename}`,
